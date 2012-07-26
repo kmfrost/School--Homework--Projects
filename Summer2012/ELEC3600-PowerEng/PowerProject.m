47fa9dcb995760bfe13fec2cebf12fb968c8941c
Markus Kreitzer <markus.kreitzer@gmail.com>
2012-07-26 16:39:54 -0500

    Final Commit for "Final Power Project"

--- Summer2012/ELEC3600-PowerEng/PowerProject.m
+++ Summer2012/ELEC3600-PowerEng/PowerProject.m
@@ -2,11 +2,12 @@
 % 2012/07/23
 % Dr. Aleck Leedy
 
-%% Problem 1.
-
 % Clear up workspace.
 clear all;
 clc;
+
+%% Problem 1.
+
 
 % Set up parameters.
 epsilon = 0.001;
@@ -44,9 +45,9 @@ end
 fprintf('\n');
 
 %% Problem 2.
+% Clean up variables from Problem 1.
+clear all;
 
-clear all
-clc
 % Initiate a N-bus system.
 N = 4; % Set N to size.
 Y = zeros(N,N); % Create Ybus matrix.
@@ -55,22 +56,30 @@ Y = zeros(N,N); % Create Ybus matrix.
 % Bus 1:
 Ya     = pol2rect(4.3914e-3,90);
 Yb     = pol2rect(1.4304e-4,90);
-% The values below are connected to ground. Node-to-Node 
+% The values here are connected to ground. Node-to-Node admittances will
+% be added later.
 Y(1,1) = Ya + Yb;
 
 % Bus 2:
+
 Yc     = pol2rect(1.4304e-4,90);
 Yd     = pol2rect(8.1177e-4,90);
+% The values here are connected to ground. Node-to-Node admittances will
+% be added later.
 Y(2,2) = Yc + Yd;
 
 % Bus 3:
 Ye     = pol2rect(4.3914e-3,90);
 Yf     = pol2rect(8.1177e-4,90);
 Yg     = pol2rect(3.3171e-3,89.95);
+% The values here are connected to ground. Node-to-Node admittances will
+% be added later.
 Y(3,3) = Ye + Yf + Yg;
 
 % Bus 4:
 Yh   = pol2rect(3.3141e-3,89.95);
+% The values here are connected to ground. Node-to-Node admittances will
+% be added later.
 Y(4,4) = Yh;
 
 % Nodal Relations:
@@ -79,29 +88,30 @@ Z12  = pol2rect(20.4941,87.11);
 Z23  = pol2rect(28.3965,87.53);
 Z34  = pol2rect(48.4066,87.63);
 
-% Convert given parameters to Admittances.
+% Convert given parameters to admittances.
 Y(1,3)  = 1/Z13;
 Y(1,2)  = 1/Z12;
 Y(2,3)  = 1/Z23;
 Y(3,4)  = 1/Z34;
 
-% Find what N in the N-bus system is.
-N = length(Y);
-
+% Fill in missing values.
 for col = 1:N
     for row = 1:N
-        Y(row,col)  =  Y(col,row); % Fill in missing values.
+        Y(row,col)  =  Y(col,row); 
     end
 end
 
+% Compute the diagonal values.
 for diag = 1:N
     %fprintf('(row,column) = (%d,%d)\n',diag,diag);
     Y(diag,diag)  = sum(Y(diag,:));
 end
 
+% Since the admittances for the non-diagonal elements are negated, we need
+% to fix that.
 for col = 1:N
     for row = 1:N
-        if(row ~= col)
+        if(row ~= col) % Non-diagonals
             Y(row,col) = -Y(row,col);
         end
     end
