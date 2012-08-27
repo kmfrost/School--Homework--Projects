cd985ccc4376cff55e741347352cc3a70a557342
Markus Kreitzer <markus.kreitzer@gmail.com>
2012-08-26 23:39:47 -0500

    Taken the semi-colons out to see what I got for each step.

--- Fall2012/EMAGII/HW4_2_madeup.m
+++ Fall2012/EMAGII/HW4_2_madeup.m
@@ -16,13 +16,13 @@ a           = 0.0005;
 b           = 0.001;
 c           = 0.004;
 %
-delta_inner       = 1/sqrt(pi*f*u*sigma_cu);
-area_inner        = pi * (a^2 - (a^2 - delta_inner)^2 );
-RAC_inner         = 1/(sigma_cu * area_inner);
+delta_inner       = 1/sqrt(pi*f*u*sigma_cu)
+area_inner        = pi * (a^2 - (a - delta_inner)^2 )
+RAC_inner         = 1/(sigma_cu * area_inner)
 
-delta_outer       = 1/sqrt(pi*f*u_st*u*sigma_st);
-area_outer        = pi * (b^2 - (b - delta_outer)^2 );
-RAC_outer         = 1/(sigma_st * area_outer);
+delta_outer       = 1/sqrt(pi*f*u_st*u*sigma_st)
+area_outer        = pi * (b^2 - (b - delta_outer)^2 )
+RAC_outer         = 1/(sigma_st * area_outer)
 
 RAC         = RAC_inner + RAC_outer;
 fprintf('AC Resistance: %f Ohms/meter\n\n',RAC);
