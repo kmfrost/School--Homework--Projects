b02ee88e7e5da2796cb4fa98dc4bb8026c73d55e
Markus Kreitzer <markus.kreitzer@gmail.com>
2012-08-26 23:23:10 -0500

    New Prob

---
+++ Fall2012/EMAGII/HW4_2_madeup.m
@@ -0,0 +1,28 @@
+% HW 4
+% Assigned Problem 2
+% Baginski made-up problem
+% 2012/08/26
+clc;
+clear all;
+fprintf('\nBaginski Probelm 2\n\n');
+%% Constants
+sigma_cu    = 5.8e7;
+sigma_st    = 5.8e6;
+u_cu       = 1;
+u_st       = 1000;
+u           = 4*pi*1e-7;
+f           = 1e9;
+a           = 0.0005;
+b           = 0.001;
+c           = 0.004;
+%
+delta_inner       = 1/sqrt(pi*f*u*sigma_cu);
+area_inner        = pi * (a^2 - (a^2 - delta_inner)^2 );
+RAC_inner         = 1/(sigma_cu * area_inner);
+
+delta_outer       = 1/sqrt(pi*f*u_st*u*sigma_st);
+area_outer        = pi * (b^2 - (b - delta_outer)^2 );
+RAC_outer         = 1/(sigma_st * area_outer);
+
+RAC         = RAC_inner + RAC_outer;
+fprintf('AC Resistance: %f Ohms/meter\n\n',RAC);
