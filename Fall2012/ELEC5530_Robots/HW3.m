6e49b0de131a60b861249c5b7e451cef139a42b4
Markus Kreitzer <markus.kreitzer@gmail.com>
2012-09-05 12:08:44 -0500

    ...

--- Fall2012/ELEC5530_Robots/HW3.m
+++ Fall2012/ELEC5530_Robots/HW3.m
@@ -6,43 +6,57 @@ clear all;
 clc;
 
 %% Declare Constants
-Zeta_I0         = [0;0;0];  % Initial position
 r             = 1;    % wheel radius
 l               = 1;    % half the vehicle wheelbase
-theta           = pi/2;
-% theta           = 0:0.1:2*pi;
-R_inv_theta     = [ cos(theta),     -sin(theta),    0;
+theta           = 0;    % start theta at zero
+phi_dot_left       = 1;   %wheel 2 (left wheel) angular rate (phi2-dot)
+%phi_dot_right       = 2;   %wheel 1 (right wheel) angular rate (phi1-dot)
+
+timeEnd             = 20;   %run the simulation for 20 seconds
+timeDiv             = 0.1;  %split the simulation into 0.1 seconds
+
+timeSteps           = (timeEnd/timeDiv)+1;  %The number of steps the simulation will have
+
+Zeta_I         = zeros(3,timeSteps);  % Initial position
+%% Run through six different scenarios where the wheel velocities vary.
+
+scenarios       = [-3,-2,-1,0,1,2,3];
+for phi_dot_right   = scenarios
+
+    theta=0;
+    Zeta_I         = zeros(3,timeSteps);  % Initial position
+
+for i = 2:1:timeSteps
+    
+    R_inv_theta     = [ cos(theta),     -sin(theta),    0;
                     sin(theta),      cos(theta),    0;
                     0,                        0,    1
                    ];
-phi_dot_1       = 1;   %wheel 2 (left wheel) angular rate (phi2-dot)
-
-%% Run through six different scenarios where the wheel velocities vary.
-scenarios       = [1 2 3 4 5 6];
-for phi_dot_2   = scenarios
-    % Velocity matrix of the 
-    w_1         = (r * phi_dot_1)/(2*l);
-    w_2         = -((r .* phi_dot_2)/(2*l));
-    Zeta_I_dot      = R_inv_theta * [ (r * phi_dot_1)/2 + (r .* phi_dot_2)/2;
+               
+    % Velocity matrix
+   Zeta_I_dot      = R_inv_theta * [ (r * phi_dot_left)/2 + (r .* phi_dot_right)/2;
                                        0;
-                                       w_1 + w_2 
+                                      (r * phi_dot_left)/(2*l) - (r * phi_dot_right)/(2*l); 
                                     ];
-    % 0 to 20 seconds
-    t               = 0:.1:20;
-    % Matrix with positions throughout time.
-    Zeta_I          = [ Zeta_I_dot(1) * cos(Zeta_I_dot(3) * t);
-                        Zeta_I_dot(2) * sin(Zeta_I_dot(3) * t);
-                        Zeta_I_dot(3) * t ];
-    x   = Zeta_I(1,:);
-    y   = Zeta_I(2,:);
-    figure
-    plot(x,y) %produces a 2-D plot
-    xlabel('x');
-    ylabel('y');
     
-    figure
-    scatter3(x,y,t) %produces a 3-D trajectory plot
-    xlabel('x');
-    ylabel('y');
-    zlabel('time');
+    theta=theta+(Zeta_I_dot(3)*timeDiv);
+    
+    Zeta_I(:,i)=Zeta_I(:,i-1)+(Zeta_I_dot*timeDiv);
+end
+
+
+figure;
+plot(Zeta_I(1,:),Zeta_I(2,:));
+axis equal
+xlabel('x')
+ylabel('y')
+
+t=0:timeDiv:timeEnd;
+figure
+scatter3(Zeta_I(1,:),Zeta_I(2,:),t); %produces a 3-D trajectory plot
+xlabel('x')
+ylabel('y')
+zlabel('time')
+
+
 end
